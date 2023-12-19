6581592a87d87342a5ce34cd4ca88b706303f025
vedprakash08 <ved9552231@gmail.com>
2023-12-19 12:58:49 +0530

    dd

---
+++ OOPs Lab/q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class temp{
+    static int count;
+    public:
+    temp(){
+        count++;
+    }
+    static void showCount(){
+        cout<<"The number of objects is "<<count<<endl;
+    }
+};
+int temp::count;
+int main()
+{
+    temp t1,t2,t3;
+    temp::showCount();
+    return 0;
+}
