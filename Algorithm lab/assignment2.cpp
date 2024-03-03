d18539b288318f2578ca37513f6cc50c3ac1bcf4
vedprakash08 <ved9552231@gmail.com>
2024-03-03 21:01:56 +0530

    d

--- Algorithm lab/assignment2.cpp
+++ Algorithm lab/assignment2.cpp
@@ -4,7 +4,6 @@ class sort{
     public:
     /***************Quick Sort*************************/
     int partitionIndex(int start,int end,int*arr){
-        int size=sizeof(arr)/sizeof(arr[0]);
         int pivot=arr[start];
         int i=start, j=end;
         while(i<=j){
@@ -79,14 +78,72 @@ class sort{
     }
 
 
+/**************************** Heap Sort ************************************************/
+void heapify(int*arr,int size,int idx){
+    
+    int largest=idx;
+    int leftChild=(2*idx)+1;
+    int rightChild=(2*idx)+2;
+
+        if(leftChild<size && arr[leftChild]>arr[largest]){
+            largest=leftChild;
+        }
+        if(rightChild<size && arr[rightChild]>arr[largest]){
+            largest=rightChild;
+        }
+        if(largest!=idx){
+            swap(arr[largest],arr[idx]);
+            heapify(arr,size,largest);
+        }
+}
+void buildHeap(int*arr,int size){
+
+    for(int i=(size/2)-1; i>=0; i--){
+        heapify(arr,size,i);
+    }
+}
+
+void heapSort(int*arr,int size){
+    buildHeap(arr,size);
+    while(size>0){
+        swap(arr[0],arr[size-1]);
+        size--;
+        heapify(arr,size,0);
+    }
+}
 };
 int main()
 {
     sort obj;
-    int arr[]={5,8,10,4,11,2};
-    int size=sizeof(arr)/sizeof(arr[0]);
-    // obj.quickSort(arr,0,size-1);
-    obj.mergeSort(arr,0,size-1);
+    int size;
+    cout<<"Enter the size of the array"<<endl;
+    cin>>size;
+    int*arr=new int[size];
+    cout<<"Enter the element of the array"<<endl;
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+    int type=0;
+    cout<<"Enter the type of sorting "<<endl;
+    cout<<"1. Quick sort"<<endl;
+    cout<<"2. Merge sort"<<endl;
+    cout<<"3. Heap sort"<<endl;
+    cin>>type;
+    switch (type)
+    {
+    case 1:
+        obj.quickSort(arr,0,size-1);
+        break;
+    case 2:
+        obj.mergeSort(arr,0,size-1);
+        break;
+    case 3:
+        obj.heapSort(arr,size);
+        break;
+    default:
+        break;
+    }
+    cout<<"SORTED ARRAY IS "<<endl;
     for(int i=0; i<size; i++) cout<<arr[i]<<" ";
     return 0;
 }
